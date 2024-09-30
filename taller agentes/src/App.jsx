bf70c0b375e46ce2ed88a4f6145f7689fb002a91
MauricioYPS <yepes060@gmail.com>
2024-09-30 18:30:52 -0500

    Nuevo proyecto

---
+++ taller agentes/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  console.log("Hola mundo");
+  
+  return (
+    <>
+      <div className='bg-slate-600 h-screen w-full border-2 border-red-300'>
+        <p>hola mundo</p>
+      </div>
+    </> 
+
+  )
+}
+
+export default App
